2afa3ff47a9f03770c36eb2c7825dc44b0c50863
WillUrban <will.urban.dev@gmail.com>
2024-03-23 22:23:02 +0700

    feat: create pure proxy

--- src/components/pure-proxy-accounts-list.tsx
+++ src/components/pure-proxy-accounts-list.tsx
@@ -2,16 +2,46 @@ import { useWalletsContext } from "@/hooks/useWalletsContext";
 import { Button } from "./ui/button";
 
 export default function PureProxyAccountsList() {
-  const { isConnected, api } = useWalletsContext();
+  const { isConnected, api, selectedAccount, wallet } = useWalletsContext();
 
-  const fetchBalance = async () => {
-    const timestamp = await api?.query.timestamp.now();
-    console.log({ timestamp });
-  };
+  const handleCreatePureProxy = async () => {
+    if (!selectedAccount) {
+      console.error("No selected account");
+      return;
+    }
 
-  const handleCreatePureProxy = () => {
-    console.log("Handle create pure proxy account.");
-    fetchBalance();
+    if (!api) {
+      console.error("No API");
+      return;
+    }
+
+    const PURE_METHOD = "PureCreated";
+
+    api.tx.proxy
+      .createPure("Any", 0, 0)
+      .signAndSend(
+        selectedAccount?.address as string,
+        {
+          signer: wallet?.signer,
+        },
+        (tx) => {
+          if (tx.status.isInBlock) {
+            tx.events.forEach(({ event: { data, method } }) => {
+              if (method === PURE_METHOD) {
+                const parsedData = data.toJSON();
+                // NOTE: This feels hacky. I'm not sure if this is the best way to check if the data is an array.
+                if (Array.isArray(parsedData)) {
+                  const [proxyAccount] = parsedData;
+                  console.log({ proxyAccount });
+                }
+              }
+            });
+          }
+        }
+      )
+      .catch((error) => {
+        console.log(":( transaction failed", error);
+      });
   };
 
   return (
